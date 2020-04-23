cc3630c5d41eb0f484e826972d9956c74f6a03da
himakkshi-17 <62936594+himakkshi-17@users.noreply.github.com>
2020-04-23 19:28:36 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// const select Element = (element) => document.querySelector(element);
+// selectElement(element:'.menu-icons').addEventListener('click'() => {
+//   selectElement.(element:'nav').classList.toggle(token:'active');
+// });
+const select Element = (element) => document.querySelector(element);
+ select Element(element:'.menu-icons').addEventListener('click'.() =>
+ { selectElement.(element:'nav').classList.toggle(token:'active');
+ });
